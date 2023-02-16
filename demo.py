10cfa9721b2c3f307d0a06ead742b3b0ef612d64
padfoot-12 <84430740+padfoot-12@users.noreply.github.com>
2023-02-17 00:02:25 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,3 +2,10 @@ def add(a,b):
     return a+b
 def subtract(a,b):
     return a-b
+def prime(x):
+    if x<=1:
+        return 0
+    for i in range(2,x):
+        if x%i==0:
+            return 0
+    return 1
